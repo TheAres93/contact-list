0ea6f63adbd146d99f6553247b92072a22c34071
LD <the_ares@hotmail.es>
2023-08-22 22:21:51 -0400

    Listo

--- tareaBasica.js
+++ tareaBasica.js
@@ -2,7 +2,7 @@ let agenda = [];
 
 let contadorId = 1;
 
-function agregarContacto(nombre, numero, correo, ciudad) {
+function agregarContacto(nombre, numero, correo, ciudad, direccion) {
   let nuevoContacto = {
     id: contadorId,
     nombre: nombre,
@@ -17,6 +17,7 @@ function agregarContacto(nombre, numero, correo, ciudad) {
   agenda.push(nuevoContacto);
   contadorId++;
   console.log(`El contacto ${nombre} ha sido agregado exitosamente.`);
+  console.log(agenda);
 }
 
 function borrarContactoPorId(id) {
@@ -31,18 +32,47 @@ function borrarContactoPorId(id) {
     if (indice !== -1) {
         console.log(`El contacto ${agenda[indice].nombre} ha sido borrado exitosamente.`);
         agenda.splice(indice, 1);
+        console.log("Lista después de borrar:", agenda)
     
     } else {
-    console.log(`El id N° ${id} no encontrado en la lista.`);
-  }}
+    console.log(`El id N° ${id} no se puede borrar en la lista.`);
+  }
+}
 
-agregarContacto("Pedro Picapiedra", "560465406", "PedritoP@YabbaDabbaDoo.com");
-agregarContacto("Pablo Marmol");
-agregarContacto("Pebbel Picapiedra");
+function actualizarContacto(idContacto, nuevoNombre, nuevoNumero, nuevoCorreo, nuevoCiudad, nuevoDireccion) {
+    let indice = -1;
+    for (let i = 0; i < agenda.length; i++) {
+        if (agenda[i].id === idContacto) {
+            indice = idContacto;
+        if (nuevoNombre !== '') {
+            agenda[i].nombre = nuevoNombre;
+        }
+        if (nuevoNumero !== '') {
+            agenda[i].numero = nuevoNumero;
+        }
+        if (nuevoCorreo !== '') {
+            agenda[i].correo = nuevoCorreo;
+        }
+        if (nuevoCiudad !== '') {
+            agenda[i].ubicacion.ciudad = nuevoCiudad;
+        }
+        if (nuevoDireccion !== '') {
+            agenda[i].ubicacion.direccion = nuevoDireccion;
+        }}    }
+        if (indice === -1){
+            console.log(`No se pudo actualizar el contacto N° ${idContacto}.`)
+        } else {
+            console.log(`Contacto N° ${idContacto} acutalizado.`, agenda)
+        }
+} 
+    
 
-console.log("Lista antes de borrar:", agenda);
-
-borrarContactoPorId(1);
-borrarContactoPorId(5);
-
-console.log("Lista después de borrar:", agenda);
+        agregarContacto("Pedro Picapiedra", 560465406, "PedritoP@YabbaDabbaDoo.com","","");
+        agregarContacto("Pablo Marmol",560465406, "PedritoP@YabbaDabbaDoo.com","","");
+        agregarContacto("Pebbel Picapiedra",560465406, "PedritoP@YabbaDabbaDoo.com","","");
+        
+        //borrarContactoPorId(1);
+        //borrarContactoPorId(5);
+ 
+        actualizarContacto(5, "", "", "kudhgfdkhfg@actuals.com","asdasf","sfgdfg");
+        actualizarContacto(2, "asdasd", "", "kudhgfdkhfg@actuals.com","asdasf","sfgdfg");
